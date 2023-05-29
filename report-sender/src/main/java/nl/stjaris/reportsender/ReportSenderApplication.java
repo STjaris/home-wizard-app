a843d4cee7af45a93a93cde46fcf627380a629c8
STjaris <43371513+STjaris@users.noreply.github.com>
2023-05-29 14:09:38 +0200

    added basics;

---
+++ report-sender/src/main/java/nl/stjaris/reportsender/ReportSenderApplication.java
@@ -0,0 +1,13 @@
+package nl.stjaris.reportsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportSenderApplication.class, args);
+	}
+
+}
